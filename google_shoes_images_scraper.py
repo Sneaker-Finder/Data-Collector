c3baaf44acf43899597750e59e4ca57bdca760dd
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-09-14 20:47:50 -0400

    minor changes

--- google_shoes_images_scraper.py
+++ google_shoes_images_scraper.py
@@ -2,24 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import json
-import base64
 
-def scrape_google_images_of_shoes_list(shoes_array: list[str])-> None:
+def scrape_google_images_of_shoes_list(shoes_array: list[str], file_count: int)-> None:
     """
     Given an array of shoes 'shoes_array', it searches the web to look for relevant for each shoe. 
     """
     options = webdriver.ChromeOptions()
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
 
     first_image = '//img[@class="YQ4gaf"]' 
     next_button_path = '//button[@jsname="OCpkoe"]'
     good_image_path = '//img[@class="sFlh5c FyHeAf iPVvYb"]'
-
+    ret = 0
     try:
         img_data = {}
         for shoe in shoes_array:
-
+            ret+=1
             img_data[shoe] = []
             search_url = "https://www.google.com/search?tbm=isch&q=" + shoe
             driver.get(search_url)
@@ -29,7 +28,12 @@ def scrape_google_images_of_shoes_list(shoes_array: list[str])-> None:
             time.sleep(3)
             
             results = driver.find_elements(By.XPATH, good_image_path)
-            
+            if (results == []):
+                time.sleep(3)
+                results = driver.find_elements(By.XPATH, good_image_path)
+            if (results == []):
+                print("No images found for " + shoe)
+                return ret
             img_data[shoe].append(results[0].get_attribute('src'))
             prev_shoe = results[0].get_attribute('src')
             
@@ -38,12 +42,18 @@ def scrape_google_images_of_shoes_list(shoes_array: list[str])-> None:
 
             next_button.click()
             counter = 0
-            while (counter < 5):
+            while (counter < 10):
                 time.sleep(2)
                 results = driver.find_elements(By.XPATH, good_image_path)
+                if (results == []):
+                    time.sleep(3)
+                    results = driver.find_elements(By.XPATH, good_image_path)
+                if (results == []):
+                    print("No images found for " + shoe)
+                    break
                 
-                if results != []:
-                    prev_shoe = results[0].get_attribute('src')
+                prev_shoe = results[0].get_attribute('src')
+                
                 for result in results:
                     link = result.get_attribute('src')
                     if link != prev_shoe:
@@ -57,20 +67,29 @@ def scrape_google_images_of_shoes_list(shoes_array: list[str])-> None:
                         buttons.click()
                         break
                 counter += 1
-                
-    except:
+    
+    except KeyboardInterrupt as e:
+         with open(f'{file_count}.json', 'w') as file:
+            json.dump(img_data, file)
+    except Exception as e:
+        print(e)
         print("error occured while retrieving images from google")
 
     # shoe_url = (img_data[shoes_array[0]][1]).split(",")[1]
     # imgdata = base64.b64decode(shoe_url)
     # with open("test/image.jpg","wb") as f:
     #     f.write(imgdata)
-        with open('shoe_img.json', 'w') as file:
+        with open(f'{file_count}Images.json', 'w') as file:
             json.dump(img_data, file)
+    with open(f'{file_count}Images.json', 'w') as file:
+        json.dump(img_data, file)   
     driver.quit()
+    return ret
 
 # Example usage:
 with open('shoe_names/stockx_format_shoes_list.txt', 'r') as shoe_names:
     shoes = [shoe.strip() for shoe in shoe_names]
+shoes = shoes[62:]
 single = ["Nike-Dunk-Low-Retro-White-Black-2021"]
-scrape_google_images_of_shoes_list(shoes)
+for x in shoes:
+    scrape_google_images_of_shoes_list([x], x)
